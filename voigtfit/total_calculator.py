f94a7bc8c51de090d70858d0bb04eff0554cea4f
svejlgaard <svejlgaard@protonmail.com>
2022-08-14 15:05:08 +0200

    Re-ordering setup and correcting numbers from pdf

--- voigtfit/total_calculator.py
+++ voigtfit/total_calculator.py
@@ -25,15 +25,15 @@ def logsum(xlist):
     return s
 
 # SUMMING THE VOIGTFIT COMPONENTS
+al = logsum([ufloat(13.21, 0.15), ufloat(13.35, 0.03), ufloat(12.24, 0.19), ufloat(12.80, 0.14)])
 c = logsum([ufloat(15.68, 0.00), ufloat(15.05, 0.0), ufloat(14.08, 0.0), ufloat(14.22, 0.0), ufloat(14.52, 0.0), ufloat(15.07, 0.0),
             ufloat(13.24, 0.14), ufloat(13.48, 0.11)])
-al = logsum([ufloat(13.21, 0.15), ufloat(13.35, 0.03), ufloat(12.24, 0.19), ufloat(12.80, 0.14)])
+fe = logsum([ufloat(14.07, 0.12), ufloat(13.98, 0.06), ufloat(12.51, 0.13), ufloat(12.68, 0.2), ufloat(13.27, 0.07), ufloat(13.64, 0.08)])
 mg = logsum([ufloat(14.31, 0.00), ufloat(15.02, 0.00), ufloat(13.42, 0.0), ufloat(13.19, 0.0), ufloat(13.43, 0.0), ufloat(12.79, 0.1)])
 o = logsum([ufloat(15.23, 0.00), ufloat(15.32, 0.00), ufloat(14.04, 0.05), ufloat(13.93, 0.06), ufloat(13.91, 0.00), ufloat(14.95, 0.00), 
             ufloat(13.63, 0.13), ufloat(13.5, 0.3), ufloat(13.23, 0.22)] )
-fe = logsum([ufloat(14.07, 0.12), ufloat(13.98, 0.06), ufloat(12.51, 0.13), ufloat(12.68, 0.2), ufloat(13.27, 0.07), ufloat(13.64, 0.08)])
 s = logsum([ufloat(14.15, 0.11), ufloat(14.24, 0.11), ufloat(13.67, 0.19)])
-si = logsum([ufloat(14.37, 0.09), ufloat(14.50, 0.04), ufloat(13.39, 0.08), ufloat(13.09, 0.04), ufloat(13.72, 0.08), ufloat(14.32, 0.12),
+si = logsum([ufloat(14.37, 0.09), ufloat(14.50, 0.04), ufloat(13.39, 0.08), ufloat(13.09, 0.04), ufloat(13.72, 0.08), ufloat(14.22, 0.12),
             ufloat(12.76,0.06), ufloat(12.52,0.10), ufloat(11.96,0.07), ufloat(12.46,0.11)])
 
 print(al, c, fe, mg, o, s, si)
@@ -42,10 +42,10 @@ print('\n')
 # SUMMING THE COMPONENTS OF ONE OF THE TWO MAJOR SYSTEMS OF LINES
 c1 = logsum([ufloat(15.68, 0.00), ufloat(15.05, 0.0), ufloat(14.08, 0.0)])
 c2 = logsum([ufloat(14.22, 0.0), ufloat(14.52, 0.0), ufloat(15.07, 0.0), ufloat(13.24, 0.14), ufloat(13.48, 0.11)])
-fe1 = logsum([ufloat(14.17, 0.14), ufloat(13.97, 0.05), ufloat(12.51, 0.13)])
+fe1 = logsum([ufloat(14.17, 0.12), ufloat(13.98, 0.06), ufloat(12.51, 0.13)])
 fe2 = logsum([ufloat(12.68, 0.2), ufloat(13.27, 0.07), ufloat(13.64, 0.08)])
-si1 = logsum([ufloat(14.37, 0.09), ufloat(14.50, 0.04), ufloat(13.24, 0.06), ufloat(12.76,0.06), ufloat(12.52,0.10)])
-si2 = logsum([ufloat(13.09, 0.04), ufloat(13.72, 0.08), ufloat(14.12, 0.10), ufloat(11.96,0.07), ufloat(12.46,0.11)])
+si1 = logsum([ufloat(14.37, 0.09), ufloat(14.50, 0.04), ufloat(13.39, 0.06), ufloat(12.76,0.06), ufloat(12.52,0.10)])
+si2 = logsum([ufloat(13.09, 0.04), ufloat(13.72, 0.08), ufloat(14.22, 0.12), ufloat(11.96,0.07), ufloat(12.46,0.11)])
 
 # COMPARING THE TWO SYSTEMS
 print(si1/fe1)
